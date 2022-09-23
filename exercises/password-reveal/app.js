c4c01123e57c05d25c22f76d12b513d91e35add2
Tetris3DS <schnetzer.ben@gmail.com>
2022-09-23 15:19:49 -0400

    Completed Password Reveal Passed Tests

--- exercises/password-reveal/app.js
+++ exercises/password-reveal/app.js
@@ -1,12 +1,13 @@
 // 🤖: create 2 constants with references to the password input and the reveal button from the DOM
-
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
 
 // 🤖: Create an Event Listener on the password reveal button
 // 🤖: The event listener should toggle the password field attribute between `password` and `text` to change the visibility of the password field content.
-
-
-// Provided for you - prevent form submit
-const form = document.querySelector("form");
-form.addEventListener('submit',function(e){
-	e.preventDefault();
+togglePassword.addEventListener('click', function (e) {
+	// toggle the type attribute
+	const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+	password.setAttribute('type', type);
+	// toggle the eye / eye slash icon
+	this.classList.toggle('bi-eye');
 });
